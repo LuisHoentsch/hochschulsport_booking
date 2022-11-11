f64b055dabe181d6543cdc264360ba55339ab9c3
qweeer <16453691+LuisHoentsch@users.noreply.github.com>
2022-11-11 13:09:18 +0100

    Add submission step

--- script.py
+++ script.py
@@ -37,22 +37,51 @@ def get_course_specific_payload(course: str, cookie: str) -> dict:
     }
 
 
+def get_submit_form_payload(course_form_data: dict, cookie: str) -> dict:
+    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/qr_info.php"
+    response = requests.post(url, course_form_data, headers={"Cookie": cookie}).text
+    if "Für diesen Kurs sind leider keine Tickets mehr verfügbar!" in response:
+        return {}
+
+    k1 = response.split('<FORM METHOD="POST" ACTION="qr_info.php" name="LoginForm">')[1].split('<INPUT TYPE="hidden" NAME="')[1].split("\"")[0]
+    v1 = response.split('<FORM METHOD="POST" ACTION="qr_info.php" name="LoginForm">')[1].split('<INPUT TYPE="hidden" NAME="' + k1 + '" VALUE="')[1].split("\"")[0]
+
+    k2 = "do"
+    v2 = "do"
+
+    k3 = "buchen"
+    v3 = "Ein für heute gültiges Ticket erstellen"
+
+    return {
+        k1: v1,
+        k2: v2,
+        k3: v3
+    }
+
+
+def submit(submit_form_data: dict, cookie: str) -> str:
+    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/qr_info.php"
+    return requests.post(url, submit_form_data, headers={"Cookie": cookie}).text
+
+
 def book(username: str, password: str, course_name: str) -> Tuple[bool, str]:
     cookie = get_authentication_cookie(username, password)
     if cookie == "":
         print("Could not authenticate user.")
         return False, ""
 
-    payload = get_course_specific_payload(course_name, cookie)
-    if payload == {}:
+    course_form_data = get_course_specific_payload(course_name, cookie)
+    if course_form_data == {}:
         print("Could not find course.")
         return False, ""
 
-    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/qr_info.php"
-    res = requests.post(url, payload, headers={"Cookie": cookie}).text
-    if "Für diesen Kurs sind leider keine Tickets mehr verfügbar!" in res:
+    submit_form_data = get_submit_form_payload(course_form_data, cookie)
+    if submit_form_data == {}:
         print("Course already fully booked.")
-        return False, res
+        return False, ""
+
+    res = submit(submit_form_data, cookie)
+
     return True, res
 
 
