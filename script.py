ac678af7679791798b78d2b2b9cfb02a6be48288
qweeer <16453691+LuisHoentsch@users.noreply.github.com>
2022-11-11 13:44:21 +0100

    Split code into two files

--- script.py
+++ script.py
@@ -1,88 +1,7 @@
 from typing import Tuple
-import requests
 import sys
 import datetime
-
-
-def get_authentication_cookie(email: str, password: str) -> str:
-    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/login.php"
-    payload = {
-        "email": email,
-        "passw": password,
-        "submit1": "Anmelden",
-        "do": "check"
-    }
-    response = requests.post(url, payload, allow_redirects=False)
-    if "Set-Cookie" not in response.headers.keys():
-        return ""  # TODO: Exception
-    return response.headers["Set-Cookie"].split(";")[0]
-
-
-def get_course_specific_payload(course: str, cookie: str) -> dict:
-    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/qr_rubriken.php"
-    response = requests.get(url, headers={"Cookie": cookie}).text
-
-    if course not in response:
-        return {}  # TODO: Exception
-
-    k1 = response.split(course)[0].split("<INPUT TYPE=\"submit\" NAME=\"")[-1].split("\"")[0]
-    v1 = "Ticket erstellen"
-
-    k2 = response.split(course)[1].split("<INPUT TYPE=\"hidden\" NAME=\"")[1].split("\"")[0]
-    v2 = response.split(course)[1].split("<INPUT TYPE=\"hidden\" NAME=\"" + k2 + "\" VALUE=\"")[1].split("\"")[0]
-
-    return {
-        k1: v1,
-        k2: v2
-    }
-
-
-def get_submit_form_payload(course_form_data: dict, cookie: str) -> dict:
-    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/qr_info.php"
-    response = requests.post(url, course_form_data, headers={"Cookie": cookie}).text
-    if "Für diesen Kurs sind leider keine Tickets mehr verfügbar!" in response:
-        return {}
-
-    k1 = response.split('<FORM METHOD="POST" ACTION="qr_info.php" name="LoginForm">')[1].split('<INPUT TYPE="hidden" NAME="')[1].split("\"")[0]
-    v1 = response.split('<FORM METHOD="POST" ACTION="qr_info.php" name="LoginForm">')[1].split('<INPUT TYPE="hidden" NAME="' + k1 + '" VALUE="')[1].split("\"")[0]
-
-    k2 = "do"
-    v2 = "do"
-
-    k3 = "buchen"
-    v3 = "Ein für heute gültiges Ticket erstellen"
-
-    return {
-        k1: v1,
-        k2: v2,
-        k3: v3
-    }
-
-
-def submit(submit_form_data: dict, cookie: str) -> str:
-    url = "https://hochschulsport.uni-heidelberg.de/oa_oeff/qr_info.php"
-    return requests.post(url, submit_form_data, headers={"Cookie": cookie}).text
-
-
-def book(username: str, password: str, course_name: str) -> Tuple[bool, str]:
-    cookie = get_authentication_cookie(username, password)
-    if cookie == "":
-        print("Could not authenticate user.")
-        return False, ""
-
-    course_form_data = get_course_specific_payload(course_name, cookie)
-    if course_form_data == {}:
-        print("Could not find course.")
-        return False, ""
-
-    submit_form_data = get_submit_form_payload(course_form_data, cookie)
-    if submit_form_data == {}:
-        print("Course already fully booked.")
-        return False, ""
-
-    res = submit(submit_form_data, cookie)
-
-    return True, res
+import business_logic
 
 
 def load_config(path: str) -> Tuple[str]:
@@ -92,9 +11,9 @@ def load_config(path: str) -> Tuple[str]:
 print(datetime.datetime.now())
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        print(book(*load_config(sys.argv[1])))
+        print(business_logic.book(*load_config(sys.argv[1])))
     elif len(sys.argv) == 4:
-        print(book(*sys.argv[1:]))
+        print(business_logic.book(*sys.argv[1:]))
     else:
         print("Usage:\n\tscript.py <email> <password> <course>\n\tscript.py <path/to/config.txt>")
 print(str(datetime.datetime.now()) + "\n\n")
